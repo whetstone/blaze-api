9f9aad20ea9a6138a946dba467a63cfcb301c03c
Greg Fiorentino <gtfiorentino@gmail.com>
2015-08-23 14:15:01 -0400

    refactored routes to separate file

--- src/index.js
+++ src/index.js
@@ -5,12 +5,7 @@ import cors from 'cors';
 import expressJwt from 'express-jwt';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-
-import * as usersController from './controllers/users-controller.js';
-import * as contactsController from './controllers/contacts-controller.js';
-import * as listsController from './controllers/lists-controller.js';
-import * as itemsController from './controllers/items-controller.js';
-import * as tokenController from './controllers/token-controller.js';
+import { setUpRoutes } from './routes/routes.js';
 
 // Uncomment these lines to synchronize models with the database at startup
 // import { syncDb } from './util/sync-db.js';
@@ -46,34 +41,7 @@ function protect() {
 
 // API Routes
 app.use(protect(), router);
-router.get('/users', usersController.fetchAllUsers);
-router.post('/users', usersController.createUser);
-router.get('/users/:userId', usersController.fetchUser);
-router.patch('/users/:userId', usersController.updateUser);
-router.delete('/users/:userId', usersController.deleteUser);
-
-router.get('/lists', listsController.fetchAllLists);
-router.get('/users/:userId/lists', listsController.fetchListsForUser);
-router.post('/lists', listsController.createList);
-router.get('/lists/:listId', listsController.fetchList);
-router.patch('/lists/:listId', listsController.updateList);
-router.delete('/lists/:listId', listsController.deleteList);
-
-router.get('/items', itemsController.fetchAllItems);
-router.get('/lists/:listId/items', itemsController.fetchItemsForList);
-router.post('/lists/:listId/items', itemsController.createItem);
-router.get('/items/:itemId', itemsController.fetchItem);
-router.patch('/items/:itemId', itemsController.updateItem);
-router.delete('/items/:itemId', itemsController.deleteItem);
-
-router.get('/contacts', contactsController.fetchAllContacts);
-router.post('/contacts', contactsController.createContact);
-router.get('/contacts/:contactId', contactsController.fetchContact);
-router.patch('/contacts/:contactId', contactsController.updateContact);
-router.delete('/contacts/:contactId', contactsController.deleteContact);
-
-router.post('/token', tokenController.createToken);
-router.delete('/token', tokenController.deleteToken);
+setUpRoutes(router);
 
 // Unauthorized Errors
 app.use(function (err, req, res, next) {
