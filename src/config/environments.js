f22e394ddf8a2923b8a35916fec4ebec5c0af630
Greg Fiorentino <gtfiorentino@gmail.com>
2015-10-04 16:14:37 -0400

    removed environments from source control

--- src/config/environments.js
+++
@@ -1,8 +0,0 @@
-const env = process.env.NODE_ENV || 'development';
-
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.DATABASE_URL = 'postgres://postgres:postgres@localhost:5432/blaze';
-  process.env.SENDGRID_USERNAME = 'sendgrid_username';
-  process.env.SENDGRID_PASSWORD = 'sendgrid_password';
-}
